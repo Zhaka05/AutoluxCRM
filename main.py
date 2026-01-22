38dc807554e06e17cd7cdb9257b19f07032f83c0
Zhaka <shingis.zhansen@gmail.com>
2026-01-21 19:14:42 -0600

    add session for database queries, lifespan for creating tables on startup

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-app = FastAPI()
+from sqlmodel import SQLModel, Session
+from .database import engine
+
+from typing import Annotated
+from contextlib import asynccontextmanager
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 templates = Jinja2Templates(directory="templates")
-
 @app.get("/")
 def main(request: Request):
     return templates.TemplateResponse(request=request, name="main.html")
